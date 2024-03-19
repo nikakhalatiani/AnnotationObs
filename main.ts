af499cbfb045c2c3a7e7bc18f0223fed5f9cc278
nikakhalatiani <khalatiani.nikolai@kiu.edu.ge>
2024-03-20 00:40:20 +0400

    first release

--- main.ts
+++ main.ts
@@ -1,40 +1,47 @@
-import { Plugin } from "obsidian";
+import { Plugin, Editor, MarkdownView } from 'obsidian';
 
-export default class ExamplePlugin extends Plugin {
-	statusBarElement: HTMLSpanElement;
+export default class MyPlugin extends Plugin {
+  async onload() {
+    this.addCommand({
+      id: 'wrap-text-with-a-tag',
+      name: 'add annotation',
+      checkCallback: (checking: boolean) => {
+        const activeLeaf = this.app.workspace.activeLeaf;
+        if (activeLeaf) {
+          const markdownView = activeLeaf.view as MarkdownView;
+          if (markdownView.getMode() === 'source' && markdownView.editor) {
+            if (!checking) {
+              this.wrapSelectedTextWithATag(markdownView.editor);
+            }
+            return true; // This command is applicable
+          }
+        }
+        return false; // This command is not applicable
+      },
+    });
+  }
+  wrapSelectedTextWithATag(editor: Editor) {
+	const selectedText = editor.getSelection();
+	if (selectedText.length === 0) return; // Do nothing if no text is selected
+  
+	// The text that will replace the selection
+	const wrappedText = `<a title="">${selectedText}</a>`;
+	const cursorPosition = editor.getCursor(); // Get the current cursor position
+  
+	// Replace the selected text with the wrapped version
+	editor.replaceSelection(wrappedText);
+  
+	// Calculate new cursor position
+	// It should be inside the title attribute, after 'title="'
+	const newPosition = {
+	  line: cursorPosition.line,
+	  ch: cursorPosition.ch + '<a title="'.length // Place the cursor right after 'title="'
+	};
+  
+	editor.setCursor(newPosition); // Set the new cursor position
+  }
 
-	onload() {
-		this.statusBarElement = this.addStatusBarItem().createEl("span");
-
-		this.readActiveFileAndUpdateLineCount();
-
-		this.app.workspace.on("editor-change", (editor) => {
-			const content = editor.getDoc().getValue();
-			this.updateLineCount(content);
-		});
-
-		this.app.workspace.on("active-leaf-change", () => {
-			this.readActiveFileAndUpdateLineCount();
-		});
-	}
-
-	onunload() {
-		this.statusBarElement.remove();
-	}
-
-	private async readActiveFileAndUpdateLineCount() {
-		const file = this.app.workspace.getActiveFile();
-		if (file) {
-			const content = await this.app.vault.read(file);
-			this.updateLineCount(content);
-		} else {
-			this.updateLineCount(undefined);
-		}
-	}
-
-	private updateLineCount(fileContent?: string) {
-		const count = fileContent ? fileContent.split(/\r\n|\r|\n/).length : 0;
-		const linesWord = count === 1 ? "line" : "lines";
-		this.statusBarElement.textContent = `${count} ${linesWord}`;
-	}
+  async onunload() {
+    console.log('unloading plugin');
+  }
 }
