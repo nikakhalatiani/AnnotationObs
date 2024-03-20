1e0434a6ad74fc637e33462673bb575711d33c6d
nikakhalatiani <khalatiani.nikolai@kiu.edu.ge>
2024-03-20 20:00:51 +0400

    fix cursor placement

--- main.ts
+++ main.ts
@@ -1,45 +1,44 @@
 import { Plugin, Editor, MarkdownView } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-  async onload() {
-    this.addCommand({
-      id: 'wrap-text-with-a-tag',
-      name: 'add annotation',
-      checkCallback: (checking: boolean) => {
-        const activeLeaf = this.app.workspace.activeLeaf;
-        if (activeLeaf) {
-          const markdownView = activeLeaf.view as MarkdownView;
-          if (markdownView.getMode() === 'source' && markdownView.editor) {
-            if (!checking) {
-              this.wrapSelectedTextWithATag(markdownView.editor);
-            }
-            return true; // This command is applicable
-          }
-        }
-        return false; // This command is not applicable
-      },
-    });
-  }
+async onload() {
+	this.addCommand({
+		id: 'wrap-text-with-a-tag',
+		name: 'add annotation',
+		checkCallback: (checking: boolean) => {
+			const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if (activeView && activeView.getMode() === 'source' && activeView.editor) {
+				if (!checking) {
+					this.wrapSelectedTextWithATag(activeView.editor);
+				}
+				return true; // This command is applicable
+			}
+			return false; // This command is not applicable
+		},
+	});
+}
   wrapSelectedTextWithATag(editor: Editor) {
 	const selectedText = editor.getSelection();
 	if (selectedText.length === 0) return; // Do nothing if no text is selected
   
 	// The text that will replace the selection
 	const wrappedText = `<a title="">${selectedText}</a>`;
-	const cursorPosition = editor.getCursor(); // Get the current cursor position
+  
+	const selectionStart = editor.getCursor('from'); // Get the start position of the selection
   
 	// Replace the selected text with the wrapped version
 	editor.replaceSelection(wrappedText);
   
-	// Calculate new cursor position
-	// It should be inside the title attribute, after 'title="'
+	// Calculate new cursor position to be right after 'title="'
+	// We use the selection start point because that's where the <a> tag starts
 	const newPosition = {
-	  line: cursorPosition.line,
-	  ch: cursorPosition.ch + '<a title="'.length // Place the cursor right after 'title="'
+	  line: selectionStart.line,
+	  ch: selectionStart.ch + '<a title="'.length,
 	};
   
 	editor.setCursor(newPosition); // Set the new cursor position
   }
+  
 
   async onunload() {
     console.log('unloading plugin');
